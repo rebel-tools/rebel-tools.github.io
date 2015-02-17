b94847634af3e01c82b006db030ac84717a91493
Nikhil Sharma <me@nikhil.sh>
2015-02-17 09:32:05 +0800

    Fixed anchor tags scrolling, modified partition

--- js/freelancer.js
+++ js/freelancer.js
@@ -9,8 +9,8 @@ $(function() {
     $('.page-scroll a').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
+            scrollTop: $($anchor.attr('href')).offset().top - 70
+        }, 1700, 'easeInOutExpo');
         event.preventDefault();
     });
 });
